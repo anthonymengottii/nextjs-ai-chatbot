f381f87283c2bf1f4e9b703845043ba46ec4fb30
Anthony Mengotti <anthonymengottii@gmail.com>
2025-12-09 08:59:23 -0300

    Adicionar nome do usuário na saudação 'Olá!'

--- components/greeting.tsx
+++ components/greeting.tsx
@@ -1,6 +1,14 @@
+"use client";
+
 import { motion } from "framer-motion";
+import { useSession } from "next-auth/react";
 
 export const Greeting = () => {
+  const { data: session } = useSession();
+  const userName = session?.user?.email
+    ? session.user.email.split("@")[0]
+    : null;
+
   return (
     <div
       className="mx-auto mt-4 flex size-full max-w-3xl flex-col justify-center px-4 md:mt-16 md:px-8"
@@ -13,7 +21,7 @@ export const Greeting = () => {
         initial={{ opacity: 0, y: 10 }}
         transition={{ delay: 0.5 }}
       >
-        Olá!
+        {userName ? `Olá, ${userName}!` : "Olá!"}
       </motion.div>
       <motion.div
         animate={{ opacity: 1, y: 0 }}
